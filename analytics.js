221e5fd0eebc437ce848edd1a92b5a87ef18eef2
EFHTools <75757412+EFHTools@users.noreply.github.com>
2021-05-11 08:23:38 -0400

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,9 +1,7 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-R4YV87LMMY"></script>
-  <script>
+
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
     gtag('config', 'G-R4YV87LMMY');
-  </script>
